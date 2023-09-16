2115f8f1e383e29e745e0fb3ec3ecab8ae315260
unimaxsystems <>
2023-09-16 17:43:05 +0530

    error

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.get('/send-review-emails', async (req, res) => {
     var email = '';
     var emailTemplate = '<!DOCTYPE html> <html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en"> <head> <title></title> <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"><!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]--> <style> * { box-sizing: border-box; } body { margin: 0; padding: 0; } a[x-apple-data-detectors] { color: inherit !important; text-decoration: inherit !important; } #MessageViewBody a { color: inherit; text-decoration: none; } p { line-height: inherit } .desktop_hide, .desktop_hide table { mso-hide: all; display: none; max-height: 0px; overflow: hidden; } .image_block img+div { display: none; } @media (max-width:620px) { .desktop_hide table.icons-inner { display: inline-block !important; } .icons-inner { text-align: center; } .icons-inner td { margin: 0 auto; } .mobile_hide { display: none; } .row-content { width: 100% !important; } .stack .column { width: 100%; display: block; } .mobile_hide { min-height: 0; max-height: 0; max-width: 0; overflow: hidden; font-size: 0px; } .desktop_hide, .desktop_hide table { display: table !important; max-height: none !important; } .row-1 .column-1 .block-2.heading_block h2 { text-align: center !important; } .row-1 .column-1 .block-2.heading_block h2 { font-size: 21px !important; } } </style> </head> <body style="background-color: #fff; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;"> <table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #fff;"> <tbody> <tr> <td> <table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tbody> <tr> <td> <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;" width="600"> <tbody> <tr> <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"> <table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tr> <td class="pad" style="width:100%;"> <div class="alignment" align="center" style="line-height:10px"><img src="https://57d858b00d.imgdist.com/public/users/Integrators/BeeProAgency/837506_821530/MaryamLogo5-2_2_300x.png" style="display: block; height: auto; border: 0; max-width: 300px; width: 100%;" width="300"></div> </td> </tr> </table> <table class="heading_block block-2" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tr> <td class="pad"> <h2 style="margin: 0; color: #000; direction: ltr; font-family: Arial, Helvetica, sans-serif; font-size: 30px; font-weight: 700; letter-spacing: normal; line-height: 120%; text-align: left; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">What did you think of your recent purchase?</span></h2> </td> </tr> </table> <table class="image_block block-3" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tr> <td class="pad" style="width:100%;"> <div class="alignment" align="center" style="line-height:10px"><img src="https://57d858b00d.imgdist.com/public/users/Integrators/BeeProAgency/837506_821530/stars.png" style="display: block; height: auto; border: 0; max-width: 183px; width: 100%;" width="183"></div> </td> </tr> </table> <table class="paragraph_block block-4" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"> <tr> <td class="pad"> <div style="color:#101112;direction:ltr;font-family:Arial, \'Helvetica Neue\', Helvetica, sans-serif;font-size:16px;font-weight:400;letter-spacing:0px;line-height:120%;text-align:center;mso-line-height-alt:19.2px;"> <p style="margin: 0;">We\'d love to hear how you enjoyed these products. Please help others by leaving a quick review of your shopping experience</p> </div> </td> </tr> </table> <table class="divider_block block-5" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tr> <td class="pad"> <div class="alignment" align="center"> <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tr> <td class="divider_inner" style="font-size: 1px; line-height: 1px; border-top: 1px solid #dddddd;"><span>&#8202;</span></td> </tr> </table> </div></td></tr></table></td> </tr> </tbody> </table> </td> </tr> </tbody> </table> {product} <table class="row row-4" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;"> <tbody> <tr> <td> <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;" width="600"> <tbody> <tr> <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"> <table class="icons_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tr> <td class="pad" style="vertical-align: middle; color: #1e0e4b; font-family: \'Inter\', sans-serif; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;"> <table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"> <tr> <td class="alignment" style="vertical-align: middle; text-align: center;"><!--[if vml]><table align="left" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]--> <!--[if !vml]><!--> <table class="icons-inner" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;" cellpadding="0" cellspacing="0" role="presentation"><!--<![endif]--> <tr> <td style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;"><a href="http://designedwithbeefree.com/" target="_blank" style="text-decoration: none;"><img class="icon" alt="Beefree Logo" src="https://d1oco4z2z1fhwp.cloudfront.net/assets/Beefree-logo.png" height="32" width="34" align="center" style="display: block; height: auto; margin: 0 auto; border: 0;"></a></td> <td style="font-family: \'Inter\', sans-serif; font-size: 15px; color: #1e0e4b; vertical-align: middle; letter-spacing: undefined; text-align: center;"></td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table><!-- End --> </body> </html>';
       // Define your SMTP server configuration
-    
+    const today = new Date();
+
 
     try {
     // Fetch order details
@@ -58,48 +59,52 @@ app.get('/send-review-emails', async (req, res) => {
      for (const orderItem of orderDetails) {     
      // const productDetails = await fetchProductDetails(obj.line_items);
         // Define email body
-        email = orderItem.customer.email; 
-        console.log(email); 
-        for(product of orderItem.line_items )
-        {
-     
-             console.log(product.product_id );
-             const product_id = product.product_id;
-             const response = await axios.get(`${productApiUrl}/${ product_id }.json`,{headers});
-             productImg = response.data.product.images[0].src;
-             productname = response.data.product.title;
-           
-             const html = '<table class="row row-4" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;"><tbody><tr><td><table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;" width="600"><tbody><tr><td class="column column-1" width="25%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"><table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"><tr><td class="pad" style="width:100%;"><div class="alignment" align="center" style="line-height:10px"><img src="'+productImg+'" style="display: block; height: auto; border: 0; max-width: 150px; width: 100%;" width="150" /></div></td></tr></table></td><td class="column column-2" width="75%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"><table class="heading_block block-1" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"><tr><td class="pad"><h2 style="margin: 0; color: #000; direction: ltr; font-family: Tahoma, Verdana, Segoe, sans-serif; font-size: 20px; font-weight: 700; letter-spacing: normal; line-height: 120%; text-align: left; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">'+productname+'</span></h2></td></tr></table><table class="button_block block-2" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"><tr><td class="pad"><div class="alignment" align="left"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://g.page/r/CW7maa3ApfR7EB0/review" style="height:29px;width:91px;v-text-anchor:middle;" arcsize="14%" stroke="false" fillcolor="#c7a3ab"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#000; font-family:Arial, sans-serif; font-size:16px"><![endif]--><a href="https://g.page/r/CW7maa3ApfR7EB0/review" target="_blank" style="text-decoration:none;display:inline-block;color:#000;background-color:#c7a3ab;border-radius:4px;width:auto;border-top:0px solid transparent;font-weight:700;border-right:0px solid transparent;border-bottom:0px solid transparent;border-left:0px solid transparent;padding-top:5px;padding-bottom:5px;font-family:Arial, \'Helvetica Neue\', Helvetica, sans-serif;font-size:16px;text-align:center;mso-border-alt:none;word-break:keep-all;"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="word-break: break-word; line-height: 19.2px;">Write a review</span></span></a></center></v:textbox></v:roundrect></div></td></tr></table></td></tr></tbody></table></td></tr></tbody></table>';
-             linehtml = linehtml + html;
-            
-        };
-        const replacedTemplate = emailTemplate.replace('{product}', linehtml); 
-        if(email){
-          await sendEmail(email,replacedTemplate);
-        }
-        // Remove the order item from the orderDetails array
-        const indexToRemove = orderDetails.indexOf(orderItem);
-        if (indexToRemove !== -1) {
-          orderDetails.splice(indexToRemove, 1);
-          await axios({
-                            method: 'PUT',
-                            url: reviewUrl,
-                            headers: {
-                              "Content-Type": "application/json",
-                              "X-Shopify-Access-Token": shopifyToken,
-                            },
-                            data: {
-                              asset: {
-                                key: reviews,
-                                value: JSON.stringify(orderDetails, null, 2),
-                              },
-                            },
-                          })
-        }
-        
-    
 
-        console.log('Emails sent successfully.');
+        const timestamp = new Date(orderItem.deliveryTimestamp);
+        if (timestamp <= today) {
+          email = orderItem.customer.email; 
+          console.log(email); 
+          for(product of orderItem.line_items )
+          {
+      
+              console.log(product.product_id );
+              const product_id = product.product_id;
+              const response = await axios.get(`${productApiUrl}/${ product_id }.json`,{headers});
+              productImg = response.data.product.images[0].src;
+              productname = response.data.product.title;
+            
+              const html = '<table class="row row-4" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;"><tbody><tr><td><table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;" width="600"><tbody><tr><td class="column column-1" width="25%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"><table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"><tr><td class="pad" style="width:100%;"><div class="alignment" align="center" style="line-height:10px"><img src="'+productImg+'" style="display: block; height: auto; border: 0; max-width: 150px; width: 100%;" width="150" /></div></td></tr></table></td><td class="column column-2" width="75%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"><table class="heading_block block-1" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"><tr><td class="pad"><h2 style="margin: 0; color: #000; direction: ltr; font-family: Tahoma, Verdana, Segoe, sans-serif; font-size: 20px; font-weight: 700; letter-spacing: normal; line-height: 120%; text-align: left; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">'+productname+'</span></h2></td></tr></table><table class="button_block block-2" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"><tr><td class="pad"><div class="alignment" align="left"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://g.page/r/CW7maa3ApfR7EB0/review" style="height:29px;width:91px;v-text-anchor:middle;" arcsize="14%" stroke="false" fillcolor="#c7a3ab"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#000; font-family:Arial, sans-serif; font-size:16px"><![endif]--><a href="https://g.page/r/CW7maa3ApfR7EB0/review" target="_blank" style="text-decoration:none;display:inline-block;color:#000;background-color:#c7a3ab;border-radius:4px;width:auto;border-top:0px solid transparent;font-weight:700;border-right:0px solid transparent;border-bottom:0px solid transparent;border-left:0px solid transparent;padding-top:5px;padding-bottom:5px;font-family:Arial, \'Helvetica Neue\', Helvetica, sans-serif;font-size:16px;text-align:center;mso-border-alt:none;word-break:keep-all;"><span style="padding-left:20px;padding-right:20px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="word-break: break-word; line-height: 19.2px;">Write a review</span></span></a></center></v:textbox></v:roundrect></div></td></tr></table></td></tr></tbody></table></td></tr></tbody></table>';
+              linehtml = linehtml + html;
+              
+          };
+          const replacedTemplate = emailTemplate.replace('{product}', linehtml); 
+          if(email){
+            await sendEmail(email,replacedTemplate);
+          }
+          // Remove the order item from the orderDetails array
+          const indexToRemove = orderDetails.indexOf(orderItem);
+          if (indexToRemove !== -1) {
+            orderDetails.splice(indexToRemove, 1);
+            await axios({
+                              method: 'PUT',
+                              url: reviewUrl,
+                              headers: {
+                                "Content-Type": "application/json",
+                                "X-Shopify-Access-Token": shopifyToken,
+                              },
+                              data: {
+                                asset: {
+                                  key: reviews,
+                                  value: JSON.stringify(orderDetails, null, 2),
+                                },
+                              },
+                            })
+          }
+          
+      
+
+          console.log('Emails sent successfully.');
+        }  
       } 
       res.status(200).json({ message: 'Data processing complete.' });
       }catch (error) {
